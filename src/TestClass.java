db0cbc93d4d2eb86ab6726eb08e14c19fe75f451
vamc <vamc.koduri@gmail.com>
2016-10-29 13:00:43 +0530

    Spring AOP Initial Setup

--- src/TestClass.java
+++
@@ -1,9 +0,0 @@
-
-public class TestClass {
-
-	public static void main(String Args[])
-	{
-	// Sample class 
-	System.out.println("Welcome to Learning Spring AOP");
-	}
-}
---
+++ src/org/javabrains/AopMain.java
@@ -0,0 +1,16 @@
+package org.javabrains;
+
+import org.javabrains.service.ShapeService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AopMain {
+
+	public static void main(String args[])
+	{
+		 ApplicationContext ctx = new ClassPathXmlApplicationContext("spring.xml");
+		 ShapeService service = ctx.getBean("shapeService",ShapeService.class);
+		 System.out.println(service.getTriangle().getName());
+	}
+	
+}
