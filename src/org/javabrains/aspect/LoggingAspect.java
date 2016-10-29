2c13dcf5366a6221087401255792e5a75796f89c
vamc <vamc.koduri@gmail.com>
2016-10-29 13:22:35 +0530

    Aspect Logging

---
+++ src/org/javabrains/aspect/LoggingAspect.java
@@ -0,0 +1,17 @@
+package org.javabrains.aspect;
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+
+@Aspect
+public class LoggingAspect {
+
+	
+	@Before("execution(public String getName())")
+	public void LoggingAdvice()
+	{
+		System.out.println("Adivce Run. Get Method Called");
+		
+	}
+}
